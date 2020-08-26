b25cea55ef03131cce253d2330bd687a0cbd0ffc
Tommy Stigen Olsen <tommysolsen@gmail.com>
2020-08-26 11:18:49 +0200

    load credentials if necessary

--- config/config.exs
+++ config/config.exs
@@ -3,3 +3,6 @@ use Mix.Config
 config :netflex, Netflex.Client, base_path: "https://api.netflexapp.com/v1/"
 
 import_config "#{Mix.env()}.exs"
+
+if File.exists?("config/#{Mix.env()}.credentials.exs"),
+  do: import_config("#{Mix.env()}.credentials.exs")
--- config/dev.exs
+++ config/dev.exs
@@ -1,3 +1,1 @@
 use Mix.Config
-
-if File.exists?("config/dev.credentials.exs"), do: import_config("#{Mix.env()}.credentials.exs")
